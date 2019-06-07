eaa8cfa71ba30c2cd3782bb136261a8df09d5d04
Djordje <46675273+djokica94@users.noreply.github.com>
2019-06-08 00:09:12 +0200

    wrong user certificate serial number ...

--- src/model/RootCA.java
+++ src/model/RootCA.java
@@ -15,18 +15,30 @@ import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.util.Base64;
 
-public class rootCA {
+import org.bouncycastle.cert.X509CRLHolder;
+
+public class RootCA {
     private static X509Certificate rootCAcertificate;
     private static PublicKey certificatePublicKey ;
     private static PrivateKey certificatePrivateKey;
+    private static X509CRLHolder crlList;
+
+    public static X509CRLHolder getCrlList() {
+        return crlList;
+    }
+
+    public static void setCrlList(X509CRLHolder crlList) {
+        RootCA.crlList = crlList;
+    }
 
     static {
         File file = new File(System.getProperty("user.dir") + File.separator + "rootCA" + File.separator + "certs" + File.separator + "ca.cer");
         try {
             rootCAcertificate = (X509Certificate) CertificateFactory.getInstance("x.509").generateCertificate(new FileInputStream(file));
-            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + File.separator + "root_CA"
-                    + File.separator + "privateKey" + File.separator + "privateKey.der")));
+            PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Files.readAllBytes(Paths.get(System.getProperty("user.dir") + File.separator + "rootCA"
+                    + File.separator + "private" + File.separator + "caprivate.der")));
             certificatePublicKey = rootCAcertificate.getPublicKey();
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             certificatePrivateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
@@ -42,5 +54,11 @@ public class rootCA {
             e.printStackTrace();
         }
 
+    }
+    public static boolean isUserOnCRLList(final User user) {
+        System.out.println("USER "+user);
+        //System.out.println("USER CERTIFICATE: "+user.getUserCertificate());
+        System.out.println("SERIAL NUMBER: "+user.getUserCertificate().getSerialNumber());
+        return crlList.getRevokedCertificate(user.getUserCertificate().getSerialNumber()) != null;
     }
 }
